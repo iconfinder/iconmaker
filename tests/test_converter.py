e28f8c8ac3f170e05c65b4edcc6605b2b1632789
Yev <yev@iconfinder.com>
2012-10-10 13:24:11 +0200

    added relative paths to test icon files

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -9,7 +9,8 @@ import unittest
 sys.path.insert(0, os.path.abspath('..'))
 from iconmaker.converter import Converter
 
-              
+ICONS_TEST_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons')
+
 class ConverterTests(unittest.TestCase):
     """Unit tests for various conversion operations.
     """
@@ -25,24 +26,22 @@ class ConverterTests(unittest.TestCase):
         """Test generation given a wrong icon format.
         """
         converter = Converter()
-        self.assertRaises(KeyError, converter.convert('foo', ['/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
-                                                              '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))        
+        self.assertRaises(KeyError, converter.convert('foo', [  os.path.join(ICONS_TEST_DIR,'icon16x16.png'),
+                                                                os.path.join(ICONS_TEST_DIR,'icon32x32.png')
+                                                            ]))        
 
     def test_convert_bad_local_pnglist(self):
         """Test generation given a bad local pnglist.
         """
         converter = Converter()
-        self.assertFalse(converter.convert('ico', [
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png',
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon64x64.png',
+        self.assertFalse(converter.convert('ico', [ os.path.join(ICONS_TEST_DIR,'icon16x16.png'),
+                                                    os.path.join(ICONS_TEST_DIR,'icon32x32.png'),
                                                     '/foo.png',
                                             ])) 
-        self.assertFalse(converter.convert('icns', [
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png',
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon64x64.png',
+        self.assertFalse(converter.convert('icns', [os.path.join(ICONS_TEST_DIR,'icon16x16.png'),
+                                                    os.path.join(ICONS_TEST_DIR,'icon32x32.png'),
                                                     '/foo.png',
                                             ]))
-
 
     def test_convert_bad_remote_pnglist(self):
         """Test generation given a bad remote pnglist.
@@ -57,16 +56,16 @@ class ConverterTests(unittest.TestCase):
                                                    'http://localhost/www/icon32x32.png',
                                             ]))
 
-
     def test_convert_local(self):
         """Test generation of ico file from local sources.
         """
         converter = Converter()
-        self.assertTrue(converter.convert('ico', [  '/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))
-        self.assertTrue(converter.convert('icns', [  '/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
-                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))
-
+        self.assertTrue(converter.convert('ico', [  os.path.join(ICONS_TEST_DIR,'icon16x16.png'),
+                                                    os.path.join(ICONS_TEST_DIR,'icon32x32.png')
+                                                ]))
+        self.assertTrue(converter.convert('icns', [  os.path.join(ICONS_TEST_DIR,'icon16x16.png'),
+                                                    os.path.join(ICONS_TEST_DIR,'icon32x32.png')
+                                                ]))
 
     def test_convert_remote(self):
         """Test generation of ico file from remote sources.

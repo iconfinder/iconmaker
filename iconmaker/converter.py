0018889b1171410e85cb87f85dcb226f14b76fda
Yev <yev@iconfinder.com>
2012-10-10 12:52:23 +0200

    reformatted and revamped tests and code

--- iconmaker/converter.py
+++ iconmaker/converter.py
@@ -3,113 +3,122 @@
 import subprocess
 import os
 import sys
-#from urllib import urlretrieve
 import utils
 import tempfile
 import logging
+import requests
+import StringIO
+from PIL import Image
+
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Converter(object):
-	"""Convert a set of PNG icons to either ICO or ICNS format
-	"""
+    """Convert a set of PNG icons to either ICO or ICNS format
+    """
 
-	"""Private method: _fetch_png
-		:params input: url of the image to fetch
-		:returns 
-	"""
-	def _fetch_png(self, url):
-		import requests
-		response = requests.get(url)
+    def _fetch_png(self, url):
+        """Private method: _fetch_png
+            :params input: url of the image to fetch
+            :returns: path to the saved_filename
+        """
 
-		import StringIO
-		from PIL import Image
-		try:
-		    im = Image.open(StringIO.StringIO(response.content))
-		    logging.debug("format: %s" % im.format)
-		    im.verify()
-		except Exception, e:
-			logging.debug("Exception: %" % e)
-			saved_filename = ''
+        response = requests.get(url)
 
-		saved_file = tempfile.NamedTemporaryFile(prefix='downloaded_png_', suffix='.png', dir='/tmp', delete=False)
-		saved_filename = saved_file.name
-		logging.debug("saving image to: %s" % saved_filename)
+        saved_file = tempfile.NamedTemporaryFile(prefix='downloaded_', suffix='.png', dir='/tmp', delete=False)
+        saved_filename = saved_file.name
 
-		im.save(saved_filename)
-		return saved_filename
+        try:
+            im = Image.open(StringIO.StringIO(response.content))
+            im.save(saved_filename)
+            logging.debug("saving image to: %s" % saved_filename)
+        except Exception, e:
+            logging.debug("problem saving image: %s to %s" % (e, saved_filename))
+            return None
 
+        return saved_filename
 
-	# initializer
-	def __init__(self):
-		"""initializer"""
+    # initializer
+    def __init__(self):
+        """initializer"""
 
-		self.png2ico = '/usr/local/bin/png2ico'
-		self.png2icns = '/usr/local/bin/png2icns'
+        self.png2ico = '/usr/local/bin/png2ico'
+        self.png2icns = '/usr/local/bin/png2icns'
 
-		"""if it doesn't exist at the path above, do a path search to find it, or throw an exception"""
-		if not os.path.isfile(self.png2ico):
-			self.png2ico = utils.which(os.path.basename(self.png2ico))
-			if not self.png2ico:
-				raise Exception("Error: png2ico binary not found")
+        """if it doesn't exist at the path above, 
+        do a path search to find it, or throw an exception
+        """
+        if not os.path.isfile(self.png2ico):
+            self.png2ico = utils.which(os.path.basename(self.png2ico))
+            if not self.png2ico:
+                raise Exception("Error: png2ico binary not found")
 
-		if not os.path.isfile(self.png2icns):
-			self.png2icns = utils.which(os.path.basename(self.png2icns))
-			if not self.png2icns:
-				raise Exception("Error: png2icns binary not found")
+        if not os.path.isfile(self.png2icns):
+            self.png2icns = utils.which(os.path.basename(self.png2icns))
+            if not self.png2icns:
+                raise Exception("Error: png2icns binary not found")
 
-		self.convert_binaries = {'ICO':self.png2ico, 'ICNS':self.png2icns}
+        self.convert_binaries = {'ICO':self.png2ico, 'ICNS':self.png2icns}
 
 
-	def convert(self, target_format, png_list):
-		"""Convert a list of png files to an ico file
-		:param target_format: ico or icns
-		:param pnglist: list of png files to convert (either local paths or URLs)
+    def convert(self, target_format, png_list):
+        """Convert a list of png files to an ico file
+        :param target_format: ico or icns
+        :param pnglist: list of png files to convert (either local paths or URLs)
 
-		:returns local path to the generated ico or None if an error occured		
-		"""
+        :returns: local path to the generated ico or None if an error occured        
+        """
 
-		logging.debug("\nconvert(): %s %s" % (target_format, png_list))
+        logging.debug("--------------------\n")
+        logging.debug("start: %s %s" % (target_format, png_list))
 
-		conversion_binary = None
-		try:
-			conversion_binary = self.convert_binaries[target_format.upper()]
-		except KeyError, e:
-			logging.debug("invalid target format: %s" % e)
-			return None
+        conversion_binary = None
+        try:
+            conversion_binary = self.convert_binaries[target_format.upper()]
+        except KeyError, e:
+            logging.debug("invalid target format: %s" % e)
+            return None
 
-		logging.debug("conversion binary: %s" % conversion_binary)
+        logging.debug("conversion binary: %s" % conversion_binary)
 
-		new_pnglist = []
-		for resource in png_list:
-			if resource.startswith("http:") or resource.startswith("https:"):
-				saved_filename = ''
-				logging.debug("Fetching PNG: %s" % resource)
+        """Loop over the elements and determine if any/all of them are http
+        if so, download the images and use these images as the new png list
+        Note: if one of the files errors out, we abort the mission
+        """
+        new_pnglist = []
+        for resource in png_list:
+            if resource.startswith("http:") or resource.startswith("https:"):
+                logging.debug("\n")
+                logging.debug("Fetching PNG: %s" % resource)
+                fetched_filename = self._fetch_png(resource)
 
-				saved_filename = self._fetch_png(resource)
+                if fetched_filename:
+                    logging.debug("Downloaded: %s" % fetched_filename)
+                    new_pnglist.append(fetched_filename)
+                else:
+                    return None
 
-				if saved_filename:
-					logging.debug("Downloaded: %s" % saved_filename)
-					new_pnglist.append(saved_filename)
+        png_list = new_pnglist if new_pnglist else png_list
 
-		png_list = new_pnglist if new_pnglist else png_list
+        if not png_list:
+            logging.debug("Empty input PNG list")           
+            return None
 
-		if not png_list:
-			logging.debug("Empty png list")			
-			return None
+        output_file = tempfile.NamedTemporaryFile(prefix='output_', suffix='.%s' % target_format, dir='/tmp', delete=False)
+        output_filename = output_file.name
+        logging.debug("output_filename: %s" % output_filename)
 
-		output_file = tempfile.NamedTemporaryFile(prefix='output_icon_', suffix='.ico', dir='/tmp', delete=False)
-		output_filename = output_file.name
-		logging.debug("output_filename: %s" % output_filename)
+        # execute shell command
+        try:
+            args = png_list
+            args.insert(0, output_filename)
+            args.insert(0, conversion_binary)
+            retcode = subprocess.call(args)
+            logging.debug("retcode: %s" % retcode)
+            if retcode != 0:
+                return None
+        except OSError, e:
+            logging.debug("Execution of conversion tool failed: %" % e)
+            return None
 
-		# execute shell command
-		try:
-			args = png_list
-			args.insert(0, output_filename)
-			args.insert(0, conversion_binary)
-			retcode = subprocess.call(args)
-		except OSError, e:
-		    logging.debug("Execution failed: %" % e)
-		    return None
-
-		return output_filename
+        return output_filename
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -9,84 +9,75 @@ import unittest
 sys.path.insert(0, os.path.abspath('..'))
 from iconmaker.converter import Converter
 
-""" Test local data
-"""
-icons_local = ['/Users/iconfinder/Iconmaker/tests/icons/package_network16x16.png',
-				'/Users/iconfinder/Iconmaker/tests/icons/package_network32x32.png',
-				'/Users/iconfinder/Iconmaker/tests/icons/package_network48x48.png'
-			  ]	
-
-icons_local_bad = ['/foo.png',
-				'/Users/iconfinder/Iconmaker/tests/icons/package_network32x32.png',
-				'/Users/iconfinder/Iconmaker/tests/icons/package_network48x48.png'
-			  ]				  
-
-""" Test remote data
-"""
-icons_remote = [
-				'http://localhost/www/icon16x16.png',
-				'http://localhost/www/icon32x32.png',
-				'http://localhost/www/icon48x48.png',
-				]			 
-
-icons_remote_bad = [
-				'http://localhost/www/icon16x16.png',
-				'http://localhost/www/icon32x32.png',
-				'http://localhost/www/foo.png',
-				]				
-
-
+              
 class ConverterTests(unittest.TestCase):
-	"""Unit tests for various conversion operations
-	"""
-	def test_convert_bad_local_pnglist(self):
-		"""Test generation given a bad local pnglist
-		"""
-		converter = Converter()
-		self.assertFalse(converter.convert('ico', icons_local_bad))	
+    """Unit tests for various conversion operations.
+    """
 
-	def test_convert_bad_remote_pnglist(self):
-		"""Test generation given a bad remote pnglist
-		"""
-		converter = Converter()
-		self.assertFalse(converter.convert('ico', icons_remote_bad))
+    def test_convert_empty_pnglist(self):
+        """Test generation given an empty pnglist.
+        """
+        converter = Converter()
+        self.assertFalse(converter.convert('ico', []))
+        self.assertFalse(converter.convert('icns', []))
 
-	def test_convert_empty_pnglist(self):
-		"""Test generation given an empty pnglist
-		"""
-		converter = Converter()
-		self.assertFalse(converter.convert('ico', []))
+    def test_convert_wrong_format(self):
+        """Test generation given a wrong icon format.
+        """
+        converter = Converter()
+        self.assertRaises(KeyError, converter.convert('foo', ['/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
+                                                              '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))        
 
-	def test_convert_wrong_format(self):
-		"""Test generation given a wrong icon format
-		"""
-		converter = Converter()
-		self.assertRaises(KeyError, converter.convert('foo', icons_local))
+    def test_convert_bad_local_pnglist(self):
+        """Test generation given a bad local pnglist.
+        """
+        converter = Converter()
+        self.assertFalse(converter.convert('ico', [
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png',
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon64x64.png',
+                                                    '/foo.png',
+                                            ])) 
+        self.assertFalse(converter.convert('icns', [
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png',
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon64x64.png',
+                                                    '/foo.png',
+                                            ]))
 
-	def test_convert_ico_local(self):
-		"""Test generation of ico file from local sources
-		"""
-		converter = Converter()
-		self.assertTrue(converter.convert('ico', icons_local))
 
-	def test_convert_ico_remote(self):
-		"""Test generation of ico file from remote sources
-		"""		
-		converter = Converter()
-		self.assertTrue(converter.convert('ico', icons_remote))
+    def test_convert_bad_remote_pnglist(self):
+        """Test generation given a bad remote pnglist.
+        """
+        converter = Converter()
+        self.assertFalse(converter.convert('ico', ['http://localhost/www/icon16x16.png',
+                                                   'http://localhost/www/foo.png',                
+                                                   'http://localhost/www/icon32x32.png',
+                                            ]))
+        self.assertFalse(converter.convert('icns', ['http://localhost/www/icon16x16.png',
+                                                   'http://localhost/www/foo.png',                
+                                                   'http://localhost/www/icon32x32.png',
+                                            ]))
 
-	def test_convert_icns_local(self):
-		"""Test generation of icns file from local sources
-		"""		
-		converter = Converter()
-		self.assertTrue(converter.convert('icns', icons_local))
 
-	def test_convert_icns_remote(self):
-		"""Test generation of icns file from remote sources
-		"""		
-		converter = Converter()
-		self.assertTrue(converter.convert('icns', icons_remote))
+    def test_convert_local(self):
+        """Test generation of ico file from local sources.
+        """
+        converter = Converter()
+        self.assertTrue(converter.convert('ico', [  '/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))
+        self.assertTrue(converter.convert('icns', [  '/Users/iconfinder/Iconmaker/tests/icons/icon16x16.png',
+                                                    '/Users/iconfinder/Iconmaker/tests/icons/icon32x32.png']))
 
+
+    def test_convert_remote(self):
+        """Test generation of ico file from remote sources.
+        """     
+        converter = Converter()
+        self.assertTrue(converter.convert('ico', ['http://localhost/www/icon16x16.png',
+                                                   'http://localhost/www/icon32x32.png',
+                                            ]))
+        self.assertTrue(converter.convert('icns', ['http://localhost/www/icon16x16.png',
+                                                   'http://localhost/www/icon32x32.png',
+                                            ]))
 
 if __name__=='__main__':
-	unittest.main()
+    unittest.main()

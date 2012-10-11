fdcb78404483b68165b2d30b2f21f910efe9e798
Yev <yev@iconfinder.com>
2012-10-11 16:56:41 +0200

    updated README.md with more instructions, revised setup.py

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 IconMaker
 =========
 
-Tool for converting icons between png, ico, and icns formats
+Tool for converting icons from gif or png to icns or ico formats.
+
+
+
+Installation
+------------
+
+To install iconmaker, first install the required dependencies.
+
+icnslib (for png to icns conversion):
+
+	$ wget http://sourceforge.net/projects/icns/files/latest/download?source=files
+	$ ./configure
+	$ make
+	$ sudo make install
+
+png2ico (for png to ico conversion):
+
+	$ wget http://www.winterdrache.de/freeware/png2ico/data/png2ico-src-2002-12-08.tar.gz
+	$ ./configure
+	$ make
+	$ sudo make install
+
+ImageMagick (for gif to png conversion):
+
+	$ wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+	$ ./configure
+	$ make
+	$ sudo make install
+
+Then, install iconmaker itself:
+
+    $ python setup.py install
--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
+import os
 from distutils.core import setup
 
+# Dynamically calculate the version based on tagging.VERSION.
+#version_tuple = __import__('iconmaker').VERSION
+#if version_tuple[2] is not None:
+#    version = "%d.%d_%s" % version_tuple
+#else:
+#    version = "%d.%d" % version_tuple[:2]
+
 setup(
-    name='iconmaker',
-    version='1.0.0',
-    author='Yev Chillen',
-    author_email='yev.chillen@iconfinder.com',
-    packages=['iconfinder', 'iconfinder.test'],
-    scripts=[],
-    url='',
-    license='LICENSE.txt',
-    description='Icon conversion utility',
-    long_description=open('README.txt').read(),
-    install_requires=[
+    name = 'iconmaker',
+    version = '1.0',
+    url = 'http://www.iconfinder.com',
+    license = 'LICENSE.txt'
+    description = 'Icon conversion utility'
+    long_description = open('README.txt').read(),
+    package_dir = {'iconmaker': 'iconmaker'},
+    packages = ['iconmaker', 
+                'iconmaker.tests'],
+    package_data = {'iconmaker':[
+                        glob('tests/icons/*'),
+                    ]},
+    install_requires = ['PIL >= 1.1.7',
+                        'requests >= 0.14.1'
     ],
 )

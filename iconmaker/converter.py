860a623e68987882179c11e41c87402b44a5899e
Yev <yev@iconfinder.com>
2012-10-16 17:23:34 +0200

    PEP8 formatting

--- iconmaker/converter.py
+++ iconmaker/converter.py
@@ -2,7 +2,6 @@
 
 import subprocess
 import os
-import sys
 import tempfile
 import requests
 import StringIO
@@ -25,9 +24,9 @@ class Converter(object):
     def _fetch_image(self, url):
         """Fetch the requested image and save it in a temporary file.
 
-            :params input: 
+            :params input:
                 URL of the image to fetch.
-            :returns: 
+            :returns:
                 Path to the saved_filename.
         """
 
@@ -38,17 +37,20 @@ class Converter(object):
         im = Image.open(StringIO.StringIO(response.content))
         image_format = im.format.lower()
         if image_format not in self.supported_source_formats:
-            raise Exception('The source file is not of a supported format. Supported formats are: %s' % 
-                            join(', ',self.supported_source_formats)) 
+            raise Exception('The source file is not of a supported format. \
+                            Supported formats are: %s' %
+                            ','.join(self.supported_source_formats))
 
         # generate temp filename for it
-        saved_file = tempfile.NamedTemporaryFile(prefix='downloaded_', suffix='.' + image_format, dir='/tmp', delete=False)
-        saved_filename = saved_file.name            
-        
+        saved_file = tempfile.NamedTemporaryFile(
+                        prefix='downloaded_',
+                        suffix='.' + image_format,
+                        dir='/tmp',
+                        delete=False)
+        saved_filename = saved_file.name
+
         im.save(saved_filename)
-
         return saved_filename
-
 
     def __init__(self):
         """Initializer.
@@ -58,7 +60,7 @@ class Converter(object):
         self.supported_target_formats = [FORMAT_ICO, FORMAT_ICNS]
         self.png2ico = '/usr/local/bin/png2ico'
         self.png2icns = '/usr/local/bin/png2icns'
-        self.gif2png = '/opt/local/bin/convert'
+        self.converttool = '/opt/local/bin/convert'
 
         # check and/or find the correct file locations
         if not os.path.isfile(self.png2ico):
@@ -71,52 +73,54 @@ class Converter(object):
             if not self.png2icns:
                 raise Exception("The binary png2icns was not found")
 
-        if not os.path.isfile(self.gif2png):
-            self.gif2png = which(os.path.basename(self.gif2png))
-            if not self.gif2png:
+        if not os.path.isfile(self.converttool):
+            self.converttool = which(os.path.basename(self.converttool))
+            if not self.converttool:
                 raise Exception("The binary gif2png was not found")
 
-        self.convert_binaries = {FORMAT_ICO:self.png2ico, FORMAT_ICNS:self.png2icns}
-
+        self.convert_binaries = {
+            FORMAT_ICO: self.png2ico,
+            FORMAT_ICNS: self.png2icns}
 
     def convert(self,
-                target_format, 
+                target_format,
                 image_list):
         """Convert a list of image files to an ico/icns file.
 
-        :param target_format: 
+        :param target_format:
             ICO or ICNS.
-        :param image_list: 
+        :param image_list:
             List of image files to convert (either local paths or URLs).
 
-        :returns: 
+        :returns:
             Local path to the generated ico or raise an Exception
         """
-
 
         # check our input arguments
         try:
             target_format = target_format.lower()
             conversion_binary = self.convert_binaries[target_format]
         except:
-            raise Exception("Invalid target format. Target format must be either ICO or ICNS.")
+            raise Exception("Invalid target format. \
+                Target format must be either ICO or ICNS.")
 
         try:
             assert len(image_list) > 0
         except:
             raise Exception("Input list cannot be empty.")
 
-
         # image_list can contain either a local path or an http url
         new_icon_list = []
         for image_location in image_list:
-            if image_location.startswith("http:") or image_location.startswith("https:"):
+            if image_location.startswith("http:") or \
+                image_location.startswith("https:"):
                 try:
                     image_location = self._fetch_image(image_location)
                 except:
                     raise Exception("Problem fetching image.")
 
-            # check the extension to see if we'll need to convert something else to PNG
+            # check the extension to see if we'll
+            # need to convert something else to PNG
             image_base, image_extension = os.path.splitext(image_location)
             image_extension = image_extension[1:]
 
@@ -125,10 +129,12 @@ class Converter(object):
                 image_location_png = "%s.%s" % (image_base, FORMAT_PNG)
 
                 try:
-                    retcode = subprocess.call([self.gif2png, image_location, image_location_png])
-                    assert retcode == 0
-                except:
-                    raise Exception('GIF to PNG conversion failed. (%s)' % image_location)
+                    subprocess.check_output([self.converttool,
+                        image_location,
+                        image_location_png])
+                except subprocess.CalledProcessError, e:
+                    raise Exception('GIF to PNG conversion failed. (%s)' %
+                        e.output)
 
                 image_location = image_location_png
 
@@ -136,63 +142,81 @@ class Converter(object):
 
         image_list = new_icon_list
 
-
         # output file in ICNS or ICO format
-        output_file = tempfile.NamedTemporaryFile(prefix='output_', suffix='.%s' % target_format, dir='/tmp', delete=False)
+        output_file = tempfile.NamedTemporaryFile(
+                        prefix='output_',
+                        suffix='.%s' % target_format,
+                        dir='/tmp',
+                        delete=False)
         output_filename = output_file.name
 
-
-        # rules for ICNS generation: (1) width must be multiple of 8 and <256. (2) Height must be <256. (3) Cannot be 64x64
-        # rules for ICO generation: (1) and (2) above
+        # Rules for ICNS generation:
+        # (1) width must be multiple of 8 and <256.
+        # (2) Height must be <256.
+        # (3) Cannot be 64x64
+        # Rules for ICO generation:
+        # (1) and
+        # (2) above
         # todo: compile Jasper libs to support 512 and 1024 icons
 
         # cache image size
         image_dict = get_image_sizes(image_list)
-        logging.debug('image dict: %s' % image_dict)
 
         # sort by size descending, and get the largest image path
-        largest_image = sorted(image_dict.items(), key=lambda x: x[1], reverse=True)[0][0]
-        logging.debug('largest image: %s' % largest_image)
+        largest_image = sorted(image_dict.items(),
+                                key=lambda x: x[1],
+                                reverse=True)[0][0]
 
         existing_sizes = sorted(image_dict.values(), reverse=True)
-        logging.debug('existing sizes: %s' % existing_sizes)
 
         largest_size = existing_sizes[0]
-        logging.debug('largest_size: %d' % largest_size)
 
         # generate the missing_sizes image sizes (by downscaling largest icon)
         #  need sizes: 16, 32, 64, 128, 256, 512, 1024
         need_sizes = [16, 32, 64, 128, 256, 512, 1024]
         missing_sizes = set(need_sizes) - set(existing_sizes)
 
-        # we'll only generate if missing sizes are less than the largest size available
+        # we'll only generate if missing sizes are
+        # less than the largest size available
         missing_sizes = [i for i in missing_sizes if i < largest_size]
-        logging.debug('missing sizes: %s (%s)' % (missing_sizes, largest_image))
+        logging.debug('missing sizes: %s (%s)' %
+            (missing_sizes, largest_image))
 
-        # todo: fill in the missing images by downscaling the `largest_image` above
+        # create the missing images by
+        # downscaling the `largest_image` above
         for m in missing_sizes:
             image_base, image_extension = os.path.splitext(largest_image)
             image_extension = image_extension[1:]
 
-            resized_file = tempfile.NamedTemporaryFile(prefix='resized_', suffix='.%s' % image_extension, dir='/tmp', delete=False)
+            new_size = "%dx%d" % (m, m)
+
+            resized_file = tempfile.NamedTemporaryFile(
+                                prefix='resized_%s' % new_size,
+                                suffix='.%s' % image_extension,
+                                dir='/tmp',
+                                delete=False)
             resized_filename = resized_file.name
 
             try:
-                new_size = "%dx%d", (m, m)
-                retcode = subprocess.call([self.gif2png, largest_image, "-resize ", new_size, resized_filename])
-                assert retcode == 0
-            except:
-                raise Exception('Image resizing failed (%s)' % image_location)
-
+                subprocess.check_output([
+                    self.converttool,
+                    largest_image,
+                    "-resize",
+                    new_size,
+                    resized_filename])
+            except subprocess.CalledProcessError, e:
+                raise Exception('Image resizing failed (%s)' % e.output)
 
         ## filter out certain icons
         if target_format == FORMAT_ICNS:
-            image_list = [i for i in image_list if image_dict[i] in need_sizes and \
-                                                    image_dict[i] != 64 and \
-                                                    image_dict[i] < 256]
+            image_list = [i for i in image_list if
+                            image_dict[i] in need_sizes and
+                            image_dict[i] != 64 and
+                            image_dict[i] < 256]
         else:
-            image_list = [i for i in image_list if image_dict[i] % 8 == 0 and \
-                                                    image_dict[i] < 256]
+            image_list = [i for i in image_list if
+                            image_dict[i] % 8 == 0 and
+                            image_dict[i] < 256]
 
         # builds args for the conversion command
         logging.debug('image list: %s' % image_list)
@@ -202,9 +226,8 @@ class Converter(object):
 
         # execute conversion command
         try:
-            retcode = subprocess.call(args)
-            assert retcode == 0
-        except:
-            raise Exception("Icon conversion failed. (%s:%s)" % (target_format, output_filename))
+            subprocess.check_output(args)
+        except subprocess.CalledProcessError, e:
+            raise Exception('failed creating an icon (%s)' % e.output)
 
         return output_filename
--- iconmaker/logger.py
+++ iconmaker/logger.py
@@ -2,5 +2,6 @@
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('converter')

accd54bbac28100bfb2d185f965de803559fb919
rathcke <nikolaj.d.rathcke@gmail.com>
2022-10-27 16:54:04 +0200

    python3 changes

--- iconmaker/converter.py
+++ iconmaker/converter.py
@@ -1,9 +1,9 @@
 import subprocess, os, tempfile, requests, struct
 
 try:
-    from cStringIO import StringIO
+    from io import StringIO
 except:
-    from StringIO import StringIO
+    from io import StringIO
 
 from .utils import check_and_get_image_sizes, which, image_mode_to_bit_depth
 from .logger import logging
@@ -452,7 +452,7 @@ class Converter(object):
 
         image_list = []
         resized_images = {}
-        for (image_size, image_path) in image_dict.iteritems():
+        for (image_size, image_path) in image_dict.items():
             (image_width, image_height) = image_size
 
             if not is_size_convertible_to_icon(image_width,
